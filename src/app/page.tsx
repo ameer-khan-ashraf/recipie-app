fec7d34d164d6d365d0544a7a0318bf04a381bc8
Ameer Khan <ameerkashraf@gmail.com>
2023-09-24 23:25:56 +0400

    fixed routing issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="container flex justify-center items-center">
       <div className="flex gap-10 my-80 text-white font-semibold">
         <Link href='/menu' className="border px-12 py-4 bg-blue-500 hover:bg-blue-600 rounded-lg">Menu</Link>
-        <Link href='/favourites' className="border px-12 py-4 bg-blue-500 hover:bg-blue-600 rounded-lg">Favourites</Link>
+        <Link href='/favourite' className="border px-12 py-4 bg-blue-500 hover:bg-blue-600 rounded-lg">Favourites</Link>
         <Link href='/random' className="border px-12 py-4 bg-blue-500 hover:bg-blue-600 rounded-lg">Generate</Link>
       </div>
     </div>
